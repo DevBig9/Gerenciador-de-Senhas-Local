84662cf8cf0fcee1e8407e16b0d75bc8c9b4c58c
BIG <contato.isaacmangelli@gmail.com>
2025-04-04 13:05:05 -0300

    Atualizei todo o arquivo main

--- Gerenciador/main.c
+++ Gerenciador/main.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include "senha.h"
 
 int main () {
-    char nome_servico [50];
-    char login [50];
-    int senha;
     int opcao;
 
+    do { 
+        printf("\n*----------------------------*\n");
+        printf("   & Gerenciador de Senhas &  \n");
+        printf("*----------------------------*\n");
 
-    printf("*----------------------------*\n");
-    printf("   &Gerenciador de Senhas&       ");
-    printf("*----------------------------*\n");
+        printf("1) Adicionar Senha\n");
+        printf("2) Listar Senhas\n");
+        printf("3) Remover Senha\n");
+        printf("0) Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
 
-    printf("Escolha alguma das opçoes abaixo para continuar: \n");
-    printf("1) Adicionar Senha.\n");
-    printf("2) Listar Senhas Salvas.\n");
-    printf("3) Remover Senhas.\n");
-    
-    if (opcao == 1) {
-        printf("1) Nome do serviço (EX: GMAIL, NETFLIX, YOUTUBE, etc...): \n");
-        scanf("%s", &nome_servico);
-        printf("2) Digite seu login(EX: cleitonrasta@gmail.com): \n");
-        scanf("%s", &login);
-        printf("3) Digite sua senha: \n");
-        scanf("%d", &senha);
-    } 
-    if (opcao == 2) {
-        printf("Lista de Senhas Salvas.\n");
-        printf("1) \n");
-        printf("2) \n");
-        printf("3) \n");
-    }
-    if (opcao == 3) {
-        printf("Remover senhas\n");
-    }
-    
-    
-   
+        switch(opcao) {
+            case 1:
+                adicionarSenha();
+                break;
+            case 2:
+                listarSenhas();
+                break;
+            case 3:
+                removerSenhas();
+                break;
+            default:
+                printf("Opção inválida.\n");
+        }
 
+    } while(opcao != 0);
+
+    return 0;
 
 }
