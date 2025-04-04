f98db86c948220a1d16fb52753e86078e4563da2
BIG <contato.isaacmangelli@gmail.com>
2025-04-04 13:02:11 -0300

    Adicionei as funcoes/voids dentro do programa

---
+++ Gerenciador/senha.h
@@ -0,0 +1,15 @@
+#ifndef SENHA_H
+#define SENHA_H
+
+typedef struct {
+    char servico [50];
+    char login [50];
+    char senha [50];
+
+} Registro;
+
+void adicionarSenha();
+void listarSenhas();
+void removerSenha();
+
+#endif
