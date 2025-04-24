c8a3bdd608e4444049230cb9324a495bac3116e7
BIG <contato.isaacmangelli@gmail.com>
2025-04-24 08:46:22 -0300

    Define cores

--- src/senha.c
+++ src/senha.c
@@ -4,6 +4,13 @@
 #include <windows.h>
 #include <time.h>
 #include "senha.h"
+#define RESET   "\033[0m"
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[1;34m"
+#define CYAN    "\033[1;36m"
+
 
 #define DB_PATH "data/senhas.txt"
 #define SENHA_MESTRE_PATH "data/senha_mestra.txt"
